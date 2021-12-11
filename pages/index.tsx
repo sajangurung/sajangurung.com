4f07dd3724feaafb38aa940d62c94a9092853b88
Sajan Gurung <sjnkon@gmail.com>
2021-12-11 21:06:34 +1100

    refactor: use array for experiences

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,57 +9,74 @@ import {
   Icon,
   ColorModeProvider,
 } from '@chakra-ui/core';
-import { Avatar, AvatarBadge } from "@chakra-ui/core";
-
+import { Avatar, AvatarBadge } from '@chakra-ui/core';
 
 export default function Home() {
   const { colorMode } = useColorMode();
+  const experiences = [
+    {
+      title: 'Findex',
+      descripton:
+        "I'm currently working as a software developer at Findex. I write code to make our internal customers workflow efficient.",
+    },
+    {
+      title: 'Propagate Digital',
+      descripton:
+        'During my 3 and half years as a full stack developer, I helped to design and deliver solutions across roster management, several e-commerce apps and custom solutions, taking full ownership from design to deployment.',
+    },
+  ];
   return (
     <ThemeProvider>
       <ColorModeProvider value={colorMode}>
-      <CSSReset />
-      <div className={styles.container}>
-        <Head>
-          <title>Sajan Gurung</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
+        <CSSReset />
+        <div className={styles.container}>
+          <Head>
+            <title>Sajan Gurung</title>
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
 
-        <main className={styles.main}>
-          <Avatar name="Sajan Gurung" src="avatar.jpg" size="xl" />
-          <h1 className={styles.title}>
-          👋 I'm Sajan
-          </h1>
+          <main className={styles.main}>
+            <Avatar name="Sajan Gurung" src="avatar.jpg" size="xl" />
+            <h1 className={styles.title}>👋 I'm Sajan</h1>
 
-          <p className={styles.description}>
-            I'm a software developer based in Melbourne, Australia.
-          </p>
-          <small>But, the picture was taken on a hike to Cradle Mountain, Tasmania.</small>
+            <p className={styles.description}>
+              I'm a software developer based in Melbourne, Australia.
+            </p>
+            <small>
+              But, the picture was taken on a hike to Cradle Mountain, Tasmania.
+            </small>
 
-
-          <div className={`${styles.grid}`}>
-            <div className={styles.card}>
-              <h3>Findex</h3>
-              <p>I'm currently working as a software developer at Findex. I write code to make our internal customers workflow efficient.</p>
+            <div className={`${styles.grid}`}>
+              {experiences.map(e => {
+                return (
+                  <div className={styles.card}>
+                    <h3>{e.title}</h3>
+                    <p>{e.descripton}</p>
+                  </div>
+                );
+              })}
+              <div className={styles.card}>
+                <h3>Skills</h3>
+                <p>Typescript, Node.js, Angular, Laravel, AWS, Jest</p>
+              </div>
             </div>
 
-            <div className={styles.card}>
-              <h3>Propagate Digital</h3>
-              <p>During my 3 and half years as a full stack developer, I helped to design and deliver solutions across roster management, several e-commerce apps and custom solutions, taking full ownership from design to deployment.</p>
+            <div className={styles.grid}>
+              <a href="https://github.com/sajangurung" className={styles.col}>
+                <img src="icons8-github.svg" alt="" />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/sajangurung/"
+                className={styles.col}
+              >
+                <img src="icons8-linkedin.svg" alt="" />
+              </a>
+              <a href="https://sajangurungk.medium.com" className={styles.col}>
+                <img src="icons8-medium.svg" alt="" />
+              </a>
             </div>
-
-            <div className={styles.card}>
-            <h3>Skills</h3>
-              <p>Typescript, Node.js, Angular, Laravel, AWS, Jest</p>
-            </div>
-          </div>
-
-          <div className={styles.grid}>
-            <a href="https://github.com/sajangurung" className={styles.col}><img src="icons8-github.svg" alt=""/></a>
-            <a href="https://www.linkedin.com/in/sajangurung/" className={styles.col}><img src="icons8-linkedin.svg" alt=""/></a>
-            <a href="https://sajangurungk.medium.com" className={styles.col}><img src="icons8-medium.svg" alt=""/></a>
-          </div>
-        </main>
-      </div>
+          </main>
+        </div>
       </ColorModeProvider>
     </ThemeProvider>
   );
